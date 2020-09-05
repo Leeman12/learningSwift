98dcf3ecb57fe05c25fefc880dc34d3575d47bb8
leeman123 <lijaa.mdhar@gmail.com>
2020-09-05 21:10:27 +0545

    Learning Variables

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+// Variable holds data, variable should be declared before using it
+// var variableName = ...
+
+var str = "Hello, playground"
+
+print(str)
+
+var a = 1
+var b = 1
+
+a = 2 // no need to re declare it once 'a' has been declared
+
+str = "hello"
+
+a = b
+
+var myNameIs = 12 // variable names are normally written in camel case
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+//Constants are similar to varibales but the values cannot be canged.
+// let a = 12
+
+let c = 12
+var d = a + c
+
+print(d)
+
+
